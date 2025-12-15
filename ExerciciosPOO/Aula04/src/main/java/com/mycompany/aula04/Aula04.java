96f2a9a017617d24d680672825d4de28f7eb0fce
Ygor Henrique <ygorlima.dev06@gmail.com>
2025-12-15 19:13:02 -0300

    ,

--- ExerciciosPOO/Aula04/src/main/java/com/mycompany/aula04/Aula04.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.aula04;
-public class Aula04 {
-    
-    public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        c1.setModelo("BIC");
-        c1.status();
-    }
-}
